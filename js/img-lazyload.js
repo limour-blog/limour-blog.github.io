ab6f139e6b4d6fbc6a85bbd70420e1933e4b3c0b
limour@limour.top <limour@limour.top>
2025-02-10 17:54:50 +0800

    Site updated: 2025-02-10 17:54:50

--- js/img-lazyload.js
+++ js/img-lazyload.js
@@ -2,6 +2,10 @@
 
 (function(window, document) {
   for (const each of document.querySelectorAll('img[lazyload]')) {
+    if(each.src.includes('/randomImg?d=')){
+      let randN = (Math.floor(Math.random() * 99) + 1).toString().padStart(2, '0');
+      each.src = `https://img.limour.top/randImg/${randN}.webp`;
+    }
     Fluid.utils.waitElementVisible(each, function() {
       each.removeAttribute('srcset');
       each.removeAttribute('lazyload');
