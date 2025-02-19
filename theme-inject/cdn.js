b0e2564f904e189dfa8d7f1cf1f5e534744d5e9a
limour@limour.top <limour@limour.top>
2025-02-19 13:41:16 +0800

    Site updated: 2025-02-19 13:41:14

--- theme-inject/cdn.js
+++ theme-inject/cdn.js
@@ -3,8 +3,9 @@ if (window.location.hostname !== oldHost && window.location.hostname !== "localh
   var newUrl = "https://" + oldHost + window.location.pathname + window.location.search + window.location.hash;
   window.location.href = newUrl;
 }
-if (window.location.hostname !== oldHost && window.location.hostname !== "localhost"){
+if (window.location.hostname !== oldHost){
   for(const each of document.querySelectorAll('script')){
+    console.log(each.src);
     if(each.src.includes('jscdn.limour.top')){
       var script = document.createElement('script');
       script.src = each.src.replace('jscdn.limour.top', 'cdn.jsdelivr.net');
