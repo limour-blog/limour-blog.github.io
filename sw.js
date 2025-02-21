9e32cf4847f735fbdffbe97df26abcb5206fa418
limour@limour.top <limour@limour.top>
2025-02-21 11:06:35 +0800

    Site updated: 2025-02-21 11:06:35

--- sw.js
+++ sw.js
@@ -4,51 +4,61 @@ const cdn_list = {
 		"https://jscdn.limour.top/npm/anchor-js@4.3.1/",
 		"https://cdn.jsdelivr.net/npm/anchor-js@4.3.1/",
 		"https://lib.baomitu.com/anchor-js/5.0.0/",
+		"https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/anchor-js/5.0.0/"
 	],
 	"github-markdown-css": [
 		"https://jscdn.limour.top/npm/github-markdown-css@4.0.0/",
 		"https://cdn.jsdelivr.net/npm/github-markdown-css@4.0.0/",
 		"https://lib.baomitu.com/github-markdown-css/4.0.0/",
+		"https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/github-markdown-css/4.0.0/"
 	],
 	"jquery": [
 		"https://jscdn.limour.top/npm/jquery@3.6.4/dist/",
 		"https://cdn.jsdelivr.net/npm/jquery@3.6.4/dist/",
 		"https://lib.baomitu.com/jquery/3.6.4/",
+		"https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/jquery/3.6.4/"
 	],
 	"bootstrap": [
 		"https://jscdn.limour.top/npm/bootstrap@4.6.1/dist/",
 		"https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/",
 		"https://lib.baomitu.com/twitter-bootstrap/4.6.1/",
+		"https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/twitter-bootstrap/4.6.1/"
 	],
 	"tocbot": [
 		"https://jscdn.limour.top/npm/tocbot@4.20.1/dist/",
 		"https://cdn.jsdelivr.net/npm/tocbot@4.20.1/dist/",
 		"https://lib.baomitu.com/tocbot/4.20.1/",
+		"https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/tocbot/4.20.1/"
 	],
 	"nprogress": [
 		"https://jscdn.limour.top/npm/nprogress@0.2.0/",
 		"https://cdn.jsdelivr.net/npm/nprogress@0.2.0/",
 		"https://lib.baomitu.com/nprogress/0.2.0/",
+		"https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/nprogress/0.2.0/"
 	],
 	"katex": [
 		"https://jscdn.limour.top/npm/katex@0.16.21/dist/",
 		"https://cdn.jsdelivr.net/npm/katex@0.16.21/dist/",
 		"https://lib.baomitu.com/KaTeX/0.16.2/",
+		"https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/KaTeX/0.16.2/"
 	],
 	"clipboard-js": [
 		"https://jscdn.limour.top/npm/clipboard-js@0.3.6/",
 		"https://cdn.jsdelivr.net/npm/clipboard-js@0.3.6/",
 		"https://lib.baomitu.com/clipboard.js/2.0.11/",
+		"https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/clipboard.js/2.0.11/"
 	],
 	"hint.css": [
 		"https://jscdn.limour.top/npm/hint.css@2.7.0/",
 		"https://cdn.jsdelivr.net/npm/hint.css@2.7.0/",
 		"https://lib.baomitu.com/hint.css/2.7.0/",
+		"https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/hint.css/2.7.0/"
 	],
 	"waline": [
-		"https://unpkg.com/@waline/client@3.5.2/dist/",
+		"https://jscdn.limour.top/npm/@waline/client@3.5.2/dist/",
 		"https://cdn.jsdelivr.net/npm/@waline/client@3.5.2/dist/",
 		"https://registry.npmmirror.com/@waline/client/3.5.2/files/dist/",
+		"https://registry.npmmirror.com/@waline/client/3.5.2/files/dist/"
 	],
 };
 const cdn_patch = [
@@ -60,7 +70,7 @@ const cdn_index = new Promise((resolve) => {
 			const url = one[0];
 			const id = one[1];
 			const startTime = performance.now();
-			return fetch(url, {method: 'GET'})
+			return fetch(url, {method: 'GET', cache: 'no-cache'})
 				.then(() => {
 					return {url, id, time: performance.now() - startTime};
 				})
@@ -71,9 +81,10 @@ const cdn_index = new Promise((resolve) => {
 		return fastest;
 	}
 	const urls = [
-		['https://cdn.jsdelivr.net/npm/anchor-js@4.3.1/anchor.min.js', 1],
-		['https://jscdn.limour.top/npm/anchor-js@4.3.1/anchor.min.js', 0],
-		['https://lib.baomitu.com/anchor-js/5.0.0/anchor.min.js', 2]
+		['https://jscdn.limour.top/npm/angular@1.8.3/angular-csp.min.css', 0],
+		['https://cdn.jsdelivr.net/npm/angular@1.8.3/angular-csp.min.css', 1],
+		['https://lib.baomitu.com/angular-i18n/1.8.3/angular-locale_af-na.min.js', 2],
+		['https://mirrors.sustech.edu.cn/cdnjs/ajax/libs/angular-i18n/1.8.3/angular-locale_af-na.min.js', 3],
 	];
     getFastestUrl(urls).then((fastest) => {
         caches.open('freecdn.limour').then( (cache) => {
@@ -103,7 +114,5 @@ onfetch = (e) => {
     if (cdn_regex.test(url.pathname)) {
         e.respondWith(new Promise( (resolve) => {
             cdn_redirect(url, resolve)
-        }
-        ))
-    }
+    }))}
 }
